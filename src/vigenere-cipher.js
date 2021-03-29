c99a2b00265879ce5fffef0f0ca41e3c78926ac0
@krikun1983 <akhmadeevst@gmail.com>
2021-03-29 22:53:40 +0400

    feat: add 10 task of 10 vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,126 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    if (type === undefined) {
+      this.type = true;
+    } else {
+      this.type = type;
+    }
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("THROWN");
+    }
+
+    const alphabet = [
+      0,
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+      newString = [];
+
+    let newMessage = message.toLowerCase();
+    let newKey = key.toLowerCase();
+
+    for (let i = 0, k = 0; i < newMessage.length; i++) {
+      let index = (alphabet.indexOf(newMessage[i]) + alphabet.indexOf(newKey[k % newKey.length]) - 1) % 26;
+      if (alphabet.indexOf(newMessage[i]) === -1) {
+        newString.push(newMessage[i]);
+      } else if (index === 0) {
+        newString.push(alphabet[26]);
+        k++;
+      } else {
+        newString.push(alphabet[index]);
+        k++;
+      }
+    }
+
+    if (this.type === false) {
+      newString.reverse();
+    }
+
+    return newString.join("").toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error("THROWN");
+    }
+
+    const alphabet = [
+      0,
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+      newString = [];
+
+    let newMessage = message.toLowerCase();
+    let newKey = key.toLowerCase();
+
+    for (let i = 0, k = 0; i < newMessage.length; i++) {
+      if (alphabet.indexOf(newMessage[i]) !== -1) {
+        let index = alphabet.indexOf(newMessage[i]) - alphabet.indexOf(newKey[k % newKey.length]) + 1;
+        index <= 0 ? newString.push(alphabet[index + 26]) : newString.push(alphabet[index]);
+        k++;
+      } else {
+        newString.push(newMessage[i]);
+      }
+    }
+
+    if (this.type === false) {
+      newString.reverse();
+    }
+    return newString.join("").toUpperCase();
   }
 }
 
